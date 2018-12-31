72cd20df9368fb7c34e7300bb76291371a5dd346
Yifeng Liu <laituan1986@gmail.com>
2018-12-31 11:32:53 +0800

    first chapter!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+// Only on MacOS.
+#[no_mangle]
+pub extern "C" fn main() -> ! {
+    loop {}
 }
