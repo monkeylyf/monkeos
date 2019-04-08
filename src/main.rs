142fac180bd28a6bfd6f158507f702d7b1bb759a
Yifeng Liu <laituan1986@gmail.com>
2019-04-08 22:24:07 +0800

    second chapter

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,29 @@
-#![no_std]
-#![no_main]
+#![no_std]  // No link to the Rust standard library.
+#![no_main]  // Disabl all Rust-level entry points
 
 use core::panic::PanicInfo;
 
+// The following function is called on panic.
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
+static HELLO: &[u8] = b"Hello world!";
+
 // Only on MacOS.
+// Do not mangle the name of following function.
 #[no_mangle]
-pub extern "C" fn main() -> ! {
+// Entry point function.
+// Linker looks for a function named "_start" by default.
+pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
     loop {}
 }
